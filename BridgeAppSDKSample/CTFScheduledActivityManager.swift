1678760135acaac4a91ea2cb0f93e3ded51b2f92
jdkizer9 <james.kizer@gmail.com>
2016-09-20 15:58:20 -0400

    added a bit of results parsing

--- BridgeAppSDKSample/CTFScheduledActivityManager.swift
+++ BridgeAppSDKSample/CTFScheduledActivityManager.swift
@@ -120,6 +120,18 @@ class CTFScheduledActivityManager: NSObject, SBASharedInfoController, ORKTaskVie
 //        return !schedule.isExpired && (!schedule.isCompleted || taskRef.allowMultipleRun)
 //    }
     
+    func scheduledActivityForTaskViewController(taskViewController: ORKTaskViewController) -> CTFScheduledActivity? {
+        guard let vc = taskViewController as? SBATaskViewController,
+            let guid = vc.scheduledActivityGUID
+            else {
+                return nil
+        }
+        return activities.findObject({ $0.guid == guid })
+    }
+    
+    func scheduledActivityForTaskIdentifier(taskIdentifier: String) -> CTFScheduledActivity? {
+        return activities.findObject({ $0.taskIdentifier == taskIdentifier })
+    }
     
     func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
         
@@ -127,22 +139,137 @@ class CTFScheduledActivityManager: NSObject, SBASharedInfoController, ORKTaskVie
 //            let schedule = scheduledActivityForTaskViewController(taskViewController)
 //            where shouldRecordResult(schedule, taskViewController: taskViewController) {
 //            
-//            // Update any data stores associated with this task
+//             Update any data stores associated with this task
 //            taskViewController.task?.updateTrackedDataStores(shouldCommit: true)
 //            
-//            // Archive the results
+//             Archive the results
 //            let results = activityResultsForSchedule(schedule, taskViewController: taskViewController)
 //            let archives = results.mapAndFilter({ archiveForActivityResult($0) })
 //            SBADataArchive.encryptAndUploadArchives(archives)
 //            
-//            // Update the schedule on the server
+//             Update the schedule on the server
 //            updateScheduledActivity(schedule, taskViewController: taskViewController)
 //        }
 //        else {
 //            taskViewController.task?.updateTrackedDataStores(shouldCommit: false)
 //        }
+        if reason == ORKTaskViewControllerFinishReason.Completed,
+            let schedule = scheduledActivityForTaskViewController(taskViewController) {
+            let results = activityResultsForSchedule(schedule, taskViewController: taskViewController)
+            print(results)
+        }
         
         taskViewController.dismissViewControllerAnimated(true) {}
+    }
+    
+    // Expose method for building results to allow for testing and subclass override
+    func activityResultsForSchedule(schedule: CTFScheduledActivity, taskViewController: ORKTaskViewController) -> [SBAActivityResult] {
+        
+        // If no results, return empty array
+        guard taskViewController.result.results != nil else { return [] }
+        
+        let taskResult = taskViewController.result
+        let surveyTask = taskViewController.task as? SBASurveyTask
+        
+        // Look at the task result start/end date and assign the start/end date for the split result
+        // based on whether or not the inputDate is greater/less than the comparison date. This way,
+        // the split result will have a start date that is >= the overall task start date and an
+        // end date that is <= the task end date.
+        func outputDate(inputDate: NSDate?, comparison:NSComparisonResult) -> NSDate {
+            let compareDate = (comparison == .OrderedAscending) ? taskResult.startDate : taskResult.endDate
+            guard let date = inputDate where date.compare(compareDate) == comparison else {
+                return compareDate
+            }
+            return date
+        }
+        
+        // Function for creating each split result
+        func createActivityResult(identifier: String, schedule: SBBScheduledActivity, stepResults: [ORKStepResult]) -> SBAActivityResult {
+            let result = SBAActivityResult(taskIdentifier: identifier, taskRunUUID: taskResult.taskRunUUID, outputDirectory: taskResult.outputDirectory)
+            result.results = stepResults
+            result.schedule = schedule
+            result.startDate = outputDate(stepResults.first?.startDate, comparison: .OrderedAscending)
+            result.endDate = outputDate(stepResults.last?.endDate, comparison: .OrderedDescending)
+            result.schemaRevision = surveyTask?.schemaRevision ?? bridgeInfo.schemaReferenceWithIdentifier(identifier)?.schemaRevision ?? 1
+            return result
+        }
+        
+        // mutable arrays for ensuring all results are collected
+        var topLevelResults:[ORKStepResult] = taskViewController.result.consolidatedResults()
+        var resultsForIdentifier: [String: ORKStepResult] = topLevelResults.reduce([String: ORKStepResult]()) { (acc, result) in
+            var returnDictionary = acc
+            returnDictionary[result.identifier] = result
+            return returnDictionary
+        }
+        print(resultsForIdentifier)
+        var allResults:[SBAActivityResult] = []
+//        var dataStores:[SBATrackedDataStore] = []
+        
+        if let task = taskViewController.task as? SBANavigableOrderedTask {
+            for step in task.steps {
+                print(step)
+                
+                if let subtaskStep = step as? SBASubtaskStep {
+                    print(subtaskStep)
+//                    var isDataCollection = false
+//                    if let subtask = subtaskStep.subtask as? SBANavigableOrderedTask,
+//                        let dataCollection = subtask.conditionalRule as? SBATrackedDataObjectCollection {
+//                        // But keep a pointer to the dataStore
+//                        dataStores.append(dataCollection.dataStore)
+//                        isDataCollection = true
+//                    }
+                    
+                    if  let taskId = subtaskStep.taskIdentifier,
+                        let schemaId = subtaskStep.schemaIdentifier {
+                        
+                        // If this is a subtask step with a schemaIdentifier and taskIdentifier
+                        // then split out the result
+                        let (subResults, filteredResults) = subtaskStep.filteredStepResults(topLevelResults)
+                        topLevelResults = filteredResults
+                        
+                        // Add filtered results to each collection as appropriate
+                        let subschedule: CTFScheduledActivity = scheduledActivityForTaskIdentifier(taskId) ?? schedule
+//                        if subResults.count > 0 {
+//                            
+//                            // add dataStore results but only if this is not a data collection itself
+//                            var subsetResults = subResults
+//                            if !isDataCollection {
+//                                for dataStore in dataStores {
+//                                    if let momentInDayResult = dataStore.momentInDayResult {
+//                                        // Mark the start/end date with the start timestamp of the first step
+//                                        for stepResult in momentInDayResult {
+//                                            stepResult.startDate = subsetResults.first!.startDate
+//                                            stepResult.endDate = stepResult.startDate
+//                                        }
+//                                        // Add the results at the beginning
+//                                        subsetResults = momentInDayResult + subsetResults
+//                                    }
+//                                }
+//                            }
+//                            
+//                            // create the subresult and add to list
+//                            let substepResult: SBAActivityResult = createActivityResult(schemaId, schedule: subschedule, stepResults: subsetResults)
+//                            allResults.append(substepResult)
+//                        }
+                    }
+//                    else if isDataCollection {
+//                        
+//                        // Otherwise, filter out the tracked object collection but do not create results
+//                        // because this is tracked via the dataStore
+//                        let (_, filteredResults) = subtaskStep.filteredStepResults(topLevelResults)
+//                        topLevelResults = filteredResults
+//                    }
+                }
+            }
+        }
+        
+        // If there are any results that were not filtered into a subgroup then include them at the top level
+//        if topLevelResults.filter({ $0.hasResults }).count > 0 {
+//            let topResult = createActivityResult(taskResult.identifier, schedule: schedule, stepResults: topLevelResults)
+//            allResults.insert(topResult, atIndex: 0)
+//        }
+        
+        return allResults
     }
     
     func didSelectRowAtIndexPath(indexPath: NSIndexPath) {
@@ -193,11 +320,40 @@ class CTFScheduledActivityManager: NSObject, SBASharedInfoController, ORKTaskVie
         return SBATaskViewController(task: task, taskRunUUID: nil)
     }
     
+}
+
+extension SBASubtaskStep {
+    func filteredTaskResult(inputResult: ORKTaskResult) -> ORKTaskResult {
+        // create a mutated copy of the results that includes only the subtask results
+        let subtaskResult: ORKTaskResult = inputResult.copy() as! ORKTaskResult
+        if let stepResults = subtaskResult.results as? [ORKStepResult] {
+            let (subtaskResults, _) = filteredStepResults(stepResults)
+            subtaskResult.results = subtaskResults
+        }
+        return subtaskResult;
+    }
     
-    
-    
-    
-    
-    
-    
+    func filteredStepResults(inputResults: [ORKStepResult]) -> (subtaskResults:[ORKStepResult], remainingResults:[ORKStepResult]) {
+        let prefix = "\(self.subtask.identifier)."
+        let predicate = NSPredicate(format: "identifier BEGINSWITH %@", prefix)
+        var subtaskResults:[ORKStepResult] = []
+        var remainingResults:[ORKStepResult] = []
+        for stepResult in inputResults {
+            if (predicate.evaluateWithObject(stepResult)) {
+                stepResult.identifier = stepResult.identifier.substringFromIndex(prefix.endIndex)
+                if let stepResults = stepResult.results {
+                    for result in stepResults {
+                        if result.identifier.hasPrefix(prefix) {
+                            result.identifier = result.identifier.substringFromIndex(prefix.endIndex)
+                        }
+                    }
+                }
+                subtaskResults += [stepResult]
+            }
+            else {
+                remainingResults += [stepResult]
+            }
+        }
+        return (subtaskResults, remainingResults)
+    }
 }
